dfe93fb2c0814a7e5d9f480167d358e5117803e1
AvecLeSSH <sadeck.brahima@etu.univ-grenoble-alpes.fr>
2024-06-10 20:38:15 +0200

    First commit

---
+++ JScript/home.js
@@ -0,0 +1,8 @@
+function toggleMenu() {
+    var menuIcon = document.querySelector('.icone-menu');
+    var menuList = document.querySelector('.barre-de-menu');
+  
+    menuIcon.classList.toggle('visible');
+    menuList.classList.toggle('visible');
+  }
+  
